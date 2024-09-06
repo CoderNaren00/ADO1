d51ff4e16abade49e2ef69e42bb7b7f59d4c7c55
_Coder_Naren <narendrajalamkar8399@gmail.com>
2024-09-06 10:37:21 +0530

    Add project files.

---
+++ AdoWebform1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AdoWebform1.aspx.cs" Inherits="ADO1.AdoWebform1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            ADO Practice<br />
+            <br />
+            <br />
+            <asp:Button ID="btnConnect" runat="server" OnClick="btnConnect_Click" Text="Connect" />
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="lblMessage" runat="server" Text="Loading..."></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AdoWebform1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO1
+{
+    public partial class AdoWebform1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("data source=ASUSNARENDRA; initial catalog=Sample1;Integrated Security=true");
+                con.Open();
+
+                lblMessage.Text = "Connection Established Successfully";
+            }
+            catch(Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+            }
+        }
+    }
+}
